6215516498fac5b9739129a0d275bae98d5562f3
asfiansaputra <4520210025@univpancasila.ac.id>
2024-05-24 00:20:50 +0700

    praktikum 8 pbm

---
+++ my_app_learning/lib/food_menu.dart
@@ -0,0 +1,11 @@
+class FoodMenu {
+  final String imageUrl;
+  final String name;
+  final int price;
+
+  FoodMenu({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+  });
+}
